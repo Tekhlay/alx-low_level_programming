23c2d6b0d812f7ef6e2778d4d826eb6a43780c3e
Tekhlay <teklaybirhane12@yahoo.com>
2022-04-09 01:26:47 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabetic.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * Main print all alphabets in lower case
+ * return 0 on success
+ */
+int main(void)
+{
+	char i;
+
+	for (i='a';i<='z';i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}

78413b4b7e14c47b155af7f7b9651cdc9e6b620e
Teklay Birhane <90598109+Tekhlay@users.noreply.github.com>
2022-04-09 10:37:14 +0300

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,15 @@
-
+#include <stdio.h>
+/**
+* main : prints all alphabets in lowercase
+* Returns 0 on Success
+*/
+int main(void){
+  char c;
+  
+  for (c='a';c<='z';c++)
+  {
+    putchar(c);
+  }
+  putchar('\n')
+  return (0);
+} 

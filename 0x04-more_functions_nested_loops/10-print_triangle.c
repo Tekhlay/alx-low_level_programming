dea85c989640835703b96636ea41b49879df179d
Teklay Birhane <90598109+Tekhlay@users.noreply.github.com>
2022-04-16 15:26:33 +0300

    Update 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,32 +1,29 @@
 #include "main.h"
 
 /**
- * print_triangle - print a triangle aligned right, using '#'
- * @size: Size of triangle
+ * print_triangle - prints a triangle .
+ * @size: size of triangle .
+ *
+ * Return: void
  */
+
+
 void print_triangle(int size)
 {
-	int c, i, j;
-
-	c = 0;
-	i = size - 1;
-	while (c < size)
-	{
-		i = size - 1 - c;
-		j = c + 1;
-		while (i > 0)
-		{
-			_putchar(' ');
-			i--;
-		}
-		while (j > 0)
-		{
-			_putchar('#');
-			j--;
-		}
-		_putchar('\n');
-		c++;
-	}
+	int i, j;
 
 	if (size <= 0)
 		_putchar('\n');
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 1; j <= size; j++)
+		{
+			if (j <= (size - i))
+				_putchar(' ');
+			else
+				_putchar('#');
+		}
+		_putchar('\n');
+	}
+
+}

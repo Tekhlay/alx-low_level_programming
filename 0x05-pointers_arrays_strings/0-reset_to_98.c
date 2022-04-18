c7d5449543762daea2c709ebb6a5ffccdd64dad4
Teklay Birhane <90598109+Tekhlay@users.noreply.github.com>
2022-04-18 15:40:39 +0300

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,16 @@
+/*
+ * Project : 0. 98 Battery st.
+ * Programmer: Mehdi Zaabi
+ * For Holberton School 2020
+ */
+
 #include "main.h"
+
 /**
- * main - check the code 
+ * reset_to_98 - resets value of pointer to 98
+ * @n: pointer to reset to 98
  *
- * Return: Always 0.
+ * Return: void
  */
 void reset_to_98(int *n)
 {

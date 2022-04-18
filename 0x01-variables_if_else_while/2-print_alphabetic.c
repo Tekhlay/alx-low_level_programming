04cf3bfc5f23ffe38d3027c22fb0b2df742a301b
Teklay Birhane <90598109+Tekhlay@users.noreply.github.com>
2022-04-18 21:44:02 +0300

    Update 2-print_alphabetic.c

--- 0x01-variables_if_else_while/2-print_alphabetic.c
+++ 0x01-variables_if_else_while/2-print_alphabetic.c
@@ -7,9 +7,12 @@
  */
 int main(void)
 {
-	int ch;
+	int ch, CH;
+
 	for (ch = 'a'; ch <= 'z'; ch++)
 		putchar(ch);
+	for (CH = 'A'; CH <= 'Z'; CH++)
+		putchar(CH);
 		putchar('\n');
 	return (0);
 }

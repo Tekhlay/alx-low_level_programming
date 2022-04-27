cd7ecafa8b48afa185628357cedd4d5719c525b0
Teklay Birhane <90598109+Tekhlay@users.noreply.github.com>
2022-04-27 18:16:06 +0300

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,22 +1,50 @@
-#include "main.h"
-#include <stdio.h>
+/**
+ * _strlen_recursion - Find the length of a string
+ * @s: The string to find length of
+ * Return: The length
+ */
+int _strlen_recursion(char *s)
+{
+	int l;
+
+	l = 0;
+	if (*s != 0)
+	{
+		l++;
+		return (l + _strlen_recursion(s + 1));
+	}
+	return (l);
+}
 
 /**
- * main - check the code
+ * pal_check - Actual check for palindrome
+ * @s: String to check
+ * @l: Length of string
  *
- * Return: Always 0.
+ * Return: 1 if palindrome, 0 if otherwise
  */
-int main(void)
+int pal_check(char *s, int l)
 {
-    int r;
+	if (l <= 1)
+		return (1);
+	if (*s != s[l - 1])
+		return (0);
+	else
+		return (pal_check((s + 1), (l - 2)));
+}
 
-    r = is_palindrome("level");
-    printf("%d\n", r);
-    r = is_palindrome("redder");
-    printf("%d\n", r);
-    r = is_palindrome("test");
-    printf("%d\n", r);
-    r = is_palindrome("step on no pets");
-    printf("%d\n", r);
-    return (0);
+/**
+ * is_palindrome - Check if a string is a palindrome
+ * @s: String to check
+ *
+ * Return: 1 if palindrome, 0 otherwise
+ */
+int is_palindrome(char *s)
+{
+	int l;
+
+	l = _strlen_recursion(s);
+	if (l <= 1)
+		return (1);
+	return (pal_check(s, l));
 }

de5f8a6a65a4b080286bcbddb68018aa026860ec
Teklay Birhane <90598109+Tekhlay@users.noreply.github.com>
2022-05-26 23:09:17 +0300

    Update 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,22 +1,23 @@
 #include "lists.h"
-#include <stdlib.h>
 
 /**
- * pop_listint - deletes the head node of a listint_t linked list
- * @head: pointer to the first element of the lsit .
+ * get_nodeint_at_index - returns the nth node of a linked list.
+ * @head: pointer to the first element of the list
+ * @index: index of the searched nod .
  *
- * Return: the head node's data (n).
+ * Return: pointer to the referenced node
  */
-int pop_listint(listint_t **head)
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t *tmp;
-	int n;
+	unsigned int i;
 
-	if (!*head)
-		return (0);
-	tmp = *head;
-	*head = tmp->next;
-	n = tmp->n;
-	free(tmp);
-	return (n);
+	if (head == NULL)
+		return (NULL);
+	for (i = 0; i < index; i++)
+	{
+		head = head->next;
+		if (head == NULL)
+			return (NULL);
+	}
+	return (head);
 }
